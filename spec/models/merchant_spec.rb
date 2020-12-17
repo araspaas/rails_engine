212e8c12da3d528b3017dc5507287508100f565e
Austin Aspaas <araspaas@live.com>
2020-12-16 18:17:50 -0700

    Add model tests for all models

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Merchant, type: :model do
-  describe 'relationships' do
-    it {should have_many :invoices}
-    it {should have_many(:customers).through(:invoices)}
-    it {should have_many(:transactions).through(:invoices)}
-    it {should have_many(:invoice_items).through(:items)}
+describe Merchant, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :name }
   end
 
-  describe 'validations' do
-    it {should validate_presence_of :name}
+  describe "relationships" do
+    it { should have_many :invoices }
+    it { should have_many :items }
   end
 end
