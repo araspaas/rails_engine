0c90bfe95d5fe4d576ce80745587653812aab940
Austin Aspaas <araspaas@live.com>
2020-12-16 19:17:34 -0700

    Add models with associations and validations

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,13 @@
 class InvoiceItem < ApplicationRecord
+  before_save :price_to_dollars
   belongs_to :item
   belongs_to :invoice
+  validates :item_id, presence: true
+  validates :invoice_id, presence: true
+  validates :quanity, presence: true
+  validates :unit_price, presence: true
+
+  def price_to_dollars
+    self.unit_price = self.unit_price / 100.00
+  end
 end
--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :invoice_item do
     association :item
     association :invoice
-    quantity { Faker::Number.number }
-    unit_price { Faker::Number.decimal }
+    quanity { 5 }
+    unit_price { 12345 }
   end
 end
