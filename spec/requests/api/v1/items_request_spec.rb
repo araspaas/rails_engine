d5a234da267508737d18905d9e01b10793c0208f
Austin Aspaas <araspaas@live.com>
2020-12-16 19:30:07 -0700

    Update test to use a serializer

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -3,13 +3,28 @@ require 'rails_helper'
 describe "Items API" do
   it "sends a list of items" do
     create_list(:item, 3)
-
     get '/api/v1/items'
-
     expect(response).to be_successful
 
     items = JSON.parse(response.body)
 
-    expect(items.count).to eq(3)
+    
+    expect(items).to be_instance_of(Hash)
+    expect(items.keys.first).to eq("data")
+    expect(items["data"]).to be_instance_of(Array)
+    expect(items["data"].count).to eq(3)
+  end
+
+  it "can get one item by its id" do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(item).to be_instance_of(Hash)
+    expect(item.keys.first).to eq("data")
+    expect(item["data"]["id"]).to eq("#{id}")
   end
 end
