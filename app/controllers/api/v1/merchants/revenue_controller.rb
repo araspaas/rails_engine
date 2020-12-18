a2ea23e845964f3f81370dcb568adc17568f659c
Austin Aspaas <araspaas@live.com>
2020-12-17 17:35:48 -0700

    Add merchants revenue controller with show action

---
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::Merchants::RevenueController < ApplicationController
+
+  def show
+    limit = params["quantity"]
+    merchants = MerchantRevenue.new.most_revenue(limit)
+    render json: MerchantSerializer.new(Merchant.find([merchants.keys]))
+  end
+end
